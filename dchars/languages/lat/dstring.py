a3c28a0008dea944302aba12a098933f901e07f2
suizokukan <suizokukan@orange.fr>
2014-01-17 16:39:52 +0100

    fixed DStringLAT.sortingvalue() : base_char is evaluated by ord(base_char) and not by base_char itself (65 < 66 rather than 'A' < 'B')

--- dchars/languages/lat/dstring.py
+++ dchars/languages/lat/dstring.py
@@ -400,7 +400,7 @@ class DStringLAT(DStringMotherClass):
             for char in self:
                 data.append( ({False:0,
                                True:1}[char.unknown_char],
-                              char.base_char )) # !!! ord
+                              ord(char.base_char) ))
             res.append(data)
 
             # length :
