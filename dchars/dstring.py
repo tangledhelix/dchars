d58783f3f6617c8015d1a05bbfdf1c6eef8b49ca
suizokukan <suizokukan@orange.fr>
2014-12-10 01:05:33 +0100

    in dchars/dstring.py, put the methods in the right order

--- dchars/dstring.py
+++ dchars/dstring.py
@@ -314,6 +314,19 @@ class DStringMotherClass(list):
         return self
 
     #///////////////////////////////////////////////////////////////////////////
+    def percentage_of_unknown_characters(self):
+        """
+                DStringMotherClass.percentage_of_unknown_characters
+
+                Return the ratio "number of unknown characters/length" or 0
+                if the object is empty.
+        """
+        if len(self)==0:
+            return 0
+
+        return 100.0*len([unknown_char for unknown_char in self if unknown_char.unknown_char==True])/len(self)
+
+    #///////////////////////////////////////////////////////////////////////////
     def read(self):
         """
                 DStringMotherClass.read
@@ -389,15 +402,3 @@ class DStringMotherClass(list):
         # to compare the two strings.
         return str(self).startswith( str(aliud) )
 
-    #///////////////////////////////////////////////////////////////////////////
-    def percentage_of_unknown_characters(self):
-        """
-                DStringMotherClass.percentage_of_unknown_characters
-
-                Return the ratio "number of unknown characters/length" or 0
-                if the object is empty.
-        """
-        if len(self)==0:
-            return 0
-
-        return 100.0*len([unknown_char for unknown_char in self if unknown_char.unknown_char==True])/len(self)
